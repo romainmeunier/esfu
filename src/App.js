a8032e25ca4eb7625e791d59b98e55d8fa30a18f
rmeunier <rmeunier@vente-privee.com>
2018-12-01 22:46:21 +0100

    fix(ci): Delete ppd fr deploy.  (TFS-000000)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
+import { ReactiveBase, CategorySearch, SingleRange, ResultCard } from '@appbaseio/reactivesearch';
 
 class App extends Component {
-
 	render() {
 		return (
-			<ReactiveBase
+				<ReactiveBase
 				app="car-store"
 				credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c">
-				// other components will go here.
-				<div>
-					Hello ReactiveSearch!
-				</div>
-			</ReactiveBase>
+					<CategorySearch
+						componentId="searchbox"
+						dataField="name"
+						categoryField="brand.raw" // use "brand.keyword" for newly cloned datasets
+						placeholder="Search for cars"
+					/>
+					<SingleRange
+						componentId="ratingsfilter"
+						title="Filter by ratings"
+						dataField="rating"
+						data={[
+							{"start": "4", "end": "5", "label": "4 stars and up"},
+							{"start": "3", "end": "5", "label": "3 stars and up"},
+							{"start": "2", "end": "5", "label": "2 stars and up"},
+							{"start": "1", "end": "5", "label": "see all ratings"},
+						]}
+						defaultSelected="4 stars and up"
+					/>
+					<ResultCard
+						componentId="result"
+						title="Results"
+						dataField="name"
+						from={0}
+						size={5}
+						pagination={true}
+						react={{
+							and: ["searchbox", "ratingsfilter"]
+						}}
+						onData={(res) => {
+							return {
+								image: "https://bit.do/demoimg",
+								title: res.name,
+								description: res.brand + " " + "★".repeat(res.rating)
+							}
+						}}
+					/>
+				</ReactiveBase>
 		);
 	}
 }
+
 export default App;
